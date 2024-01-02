798fd251eced2a4a414b452470c9fc12c20de69e
harshaBangaram <155428809+harshaBangaram@users.noreply.github.com>
2024-01-02 14:00:01 +0530

    Add files via upload

---
+++ BiConsumer1.java
@@ -0,0 +1,15 @@
+package biConsumer;
+import java.util.function.*;
+public class BiConsumer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BiConsumer<Integer,Integer> b=(a,c)->a.compareTo(c);{
+	
+	System.out.println(b);
+	
+}b.accept(2, 3);
+
+	}
+
+}
